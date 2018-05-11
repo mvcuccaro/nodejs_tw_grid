942aa23258e2163d63bc2598a14ada659a5ad6b2
mvcuccaro <mvcuccaro@users.noreply.github.com>
2018-05-10 23:06:17 -0400

    added different grid init types to twmain.js driver

--- src/twmain.js
+++ src/twmain.js
@@ -1,27 +1,28 @@
-var twgrid = require('./twgrid')({cell_count:20, items_per_row:4});
-console.log('A custom size random grid');
-twgrid.drawGrid();
+var twgrid = require('./twgrid');
+mygrid = twgrid({cell_count:20, items_per_row:4});
+console.log('A custom size random grid\n');
+mygrid.drawGrid();
 
-console.log('\n\nA static grid');
-twgrid = require('./twgrid')({grid: [[1,2,3], [4,5,6]]});
-twgrid.drawGrid();
+console.log('\nA static grid\n');
+mygrid = twgrid({grid: [[1,2,3], [4,5,6]]});
+mygrid.drawGrid();
 
 
 console.log('a hardcoded random grid')
-twgrid = require('./twgrid')();
+mygrid = twgrid();;
 //Draw the grid
 console.log("\nHardcoded size Random Grid:\n");
-twgrid.drawGrid();
+mygrid.drawGrid();
 
 
 
-console.log("\n\n");
+console.log("\n");
 
 //get coordinates for a sector
-console.log('The coordinates of sector 23 are ', twgrid.getCoordinatesByValue(23));
+console.log('The coordinates of sector 23 are ', mygrid.getCoordinatesByValue(23));
 
 //get sector from a coordinate pair
-console.log('The sector at coordinates 2,3 are ', twgrid.getValueByCoordinates([2,3]));
+console.log('The sector at coordinates 2,3 are ', mygrid.getValueByCoordinates([2,3]));
 
 //get route from 2 sectors
-console.log('The route from sector 24 to 11 is', twgrid.getRoute(24, 11));
+console.log('The route from sector 24 to 11 is', mygrid.getRoute(24, 11));
